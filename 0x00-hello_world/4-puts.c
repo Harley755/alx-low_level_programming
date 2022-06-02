654b2346502634d4cbc21a387a8e464f9ac42f45
Brice <bricegoudalo@gmail.com>
2022-06-02 16:56:59 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
 /**
  * started by main function
  * used puts function to display text
- * return 0 for success
+ * return: 0 for success
  */
-
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
