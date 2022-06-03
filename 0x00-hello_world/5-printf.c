8c3fb9bf591c0a6cdba37928edc15dca1660c27a
Brice <bricegoudalo@gmail.com>
2022-06-02 17:11:32 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h/>
+/**
+ * main - main function (entry point)
+ * used printf function to display text
+ * Return: 0 for success
+ */
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}
