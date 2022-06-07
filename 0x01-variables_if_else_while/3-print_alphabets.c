c9e36eecbb10cb418d6ce79a97497a0adaadaedc
Brice <bricegoudalo@gmail.com>
2022-06-06 17:26:05 -0700

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+nclude <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Display all alphabet letters
+ * Return: 0 for Success
+ */
+int main(void)
+{
+	char c = 'a';
+	int count;
+	do {
+		putchar(c);
+		if (c == 'z')
+		{
+			c = 'A';
+			count = 0;
+		}
+		else
+		{
+			count++;
+			c++;
+		}
+	} while (count < 26);
+	putchar('\n');
+	return (0);
+}
