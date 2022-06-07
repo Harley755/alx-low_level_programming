e2f9ebb04c419e9b1f4f4d1dd82ee083dbb63ca3
Brice <bricegoudalo@gmail.com>
2022-06-06 17:47:44 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main function 
+ * Display all single digit numbers of base 10
+ * Return: 0 for Success
+ */
+int main(void)
+{
+	int i = 48;
+
+	for (; i <= 57; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
