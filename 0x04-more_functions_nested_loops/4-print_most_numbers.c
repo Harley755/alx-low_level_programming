b464f99c4d40c2ac3069735a62ad1de3eee2f260
Brice <bricegoudalo@gmail.com>
2022-06-13 17:40:55 -0700

    4. I believe in numbers and signs

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Display numbers with _putchar except 2 && 4
+ * Display numbers except 2 and 4
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		if (i != 50 && i != 52)
+			putchar(i);
+	putchar('\n');
+}
