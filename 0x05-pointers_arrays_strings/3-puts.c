4fe9b57dc4220c5a23ceed04e2fb5660e1a66247
Brice <bricegoudalo@gmail.com>
2022-06-13 17:02:56 -0700

    3. I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - display a string
+ * @str: a string
+ * Get the length of a string
+ * Return: length of the string
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
