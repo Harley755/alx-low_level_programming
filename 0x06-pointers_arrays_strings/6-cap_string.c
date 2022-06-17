031ddf45998c60e7ec06eda7b69c168636679f54
Brice <bricegoudalo@gmail.com>
2022-06-16 18:15:33 -0700

    cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,7 @@ char *cap_string(char *str)
 	int i;
 
 	for (i = 0; str[i]; i++)
-	{	
+	{
 		if ((str[i] >= 'a' && str[i] <= 'z') &&
 				(str[i - 1] == '!' || str[i - 1] == ' ' ||
 				str[i - 1] == '\t' || str[i - 1] == '\n' ||
