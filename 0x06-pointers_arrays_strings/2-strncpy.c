5afa26719011522b47ab4992a416b903efde6825
Brice <bricegoudalo@gmail.com>
2022-06-16 17:09:51 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,11 +10,6 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
-
-	for (i = 0; i < n && src[i]; i++)
-		dest[i] = src[i];
-		for (; i < n; i++)
-			dest[i] = '\0';
+	strncpy(dest, src, n);
 	return (dest);
 }
