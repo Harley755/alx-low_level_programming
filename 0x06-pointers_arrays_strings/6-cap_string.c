a0afd19395cb4a2a732c8eca3eb95783907d6ac1
Brice <bricegoudalo@gmail.com>
2022-06-16 18:14:26 -0700

    cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,23 +10,20 @@ char *cap_string(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if ((str[i] == ',' ||
-			str[i] == ';' ||
-			str[i] == '.' ||
-			str[i] == '!' ||
-			str[i] == '?' ||
-			str[i] == '"' ||
-			str[i] == '(' ||
-			str[i] == ')' ||
-			str[i] == '{' ||
-			str[i] == '}' ||
-			str[i] == ' ' ||
-			str[i] == '\t' ||
-			str[i] == '\n' ||
-			str[i] == ' ') && str[i + 1] >= 97 && str[i + 1] <=122)
-			str[i + 1] -= 32;
+	for (i = 0; str[i]; i++)
+	{	
+		if ((str[i] >= 'a' && str[i] <= 'z') &&
+				(str[i - 1] == '!' || str[i - 1] == ' ' ||
+				str[i - 1] == '\t' || str[i - 1] == '\n' ||
+				str[i - 1] == '(' || str[i - 1] == ')' ||
+				str[i - 1] == '?' || str[i - 1] == '"' ||
+				str[i - 1] == '{' || str[i - 1] == '}' ||
+				str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.'))
+			str[i] -= 32;
+		else if (str[0] >= 'a' && str[0] <= 122)
+			str[i] -= 32;
+		else
+			continue;
 	}
 	return (str);
 }
